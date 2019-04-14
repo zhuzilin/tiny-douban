e6d49ea92189ca77645828e4bfb0d515be50a62f
zhuzilin <zhuzilinallen@gmail.com>
2019-04-14 18:43:30 -0400

    add 404 page

--- src/server.py
+++ src/server.py
@@ -418,6 +418,13 @@ where customer_id = {} and movie_id = {}
     res.status_code = 200
     return res
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     import click
 
